cc5c65d4491ae0a4028c776c7225c969815c5285
Maurenys Varela <57731222+maurenys@users.noreply.github.com>
2022-04-15 12:36:03 -0400

    Add files via upload

---
+++ ProyectoSpringBodegaModeulo6MAURENYS/src/main/java/cl/icap/fullstackjava2022/modulo6/controller/WebAppController.java
@@ -0,0 +1,23 @@
+package cl.icap.fullstackjava2022.modulo6.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebAppController {
+	@RequestMapping("/")
+	public String getHome() {
+		return("index");
+	}
+	
+	@RequestMapping("/movimiento_page")
+	public String getMovimiento() {
+		return("movimiento");
+}
+	
+	@RequestMapping("/lote_page")
+	public String getLote() {
+		return("lote");
+}
+	
+}
